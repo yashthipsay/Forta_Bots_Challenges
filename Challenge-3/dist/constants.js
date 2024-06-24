26fcb9a51bce50c83dc828537ebb6a1f010df4e5
Yash Thipsay <yashthpsay@gmail.com>
2024-06-24 12:00:58 +0530

    new branch

--- Challenge-3/dist/constants.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.DAI_L2_ADDRESS = exports.DAI_ADDRESS = exports.ESCROW_ABI = exports.OPT_ESCROW_ADDRESS = exports.ABT_ESCROW_ADDRESS = exports.L2_ABI = void 0;
-exports.L2_ABI = "function totalSupply() view returns (uint256)";
-exports.ABT_ESCROW_ADDRESS = "0xA10c7CE4b876998858b1a9E12b10092229539400";
-exports.OPT_ESCROW_ADDRESS = "0x467194771dAe2967Aef3ECbEDD3Bf9a310C76C65";
-exports.ESCROW_ABI = 'function balanceOf(address) view returns (uint256)';
-exports.DAI_ADDRESS = "0x6B175474E89094C44Da98b954EedeAC495271d0F";
-exports.DAI_L2_ADDRESS = "0xDA10009cBd5D07dd0CeCc66161FC93D7c9000da1";
