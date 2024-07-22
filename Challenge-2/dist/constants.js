68bfe4638be6b43e1694b969b51c2686aeb02f5b
Yash Thipsay <yashthpsay@gmail.com>
2024-07-22 18:43:01 +0530

    removing node_modules

--- Challenge-2/dist/constants.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.NETHERMIND_DEPLOYER_ADDRESS = exports.FORTA_BOT_REGISTRY = exports.UPDATE_BOT_FUNCTION = exports.CREATE_BOT_FUNCTION = void 0;
-exports.CREATE_BOT_FUNCTION = "function createAgent(uint256 agentId, address, string metadata, uint256[] chainIds)";
-exports.UPDATE_BOT_FUNCTION = "function updateAgent(uint256 agentId, string metadata, uint256[] chainIds)";
-exports.FORTA_BOT_REGISTRY = "0x61447385B019187daa48e91c55c02AF1F1f3F863";
-exports.NETHERMIND_DEPLOYER_ADDRESS = "0x88dC3a2284FA62e0027d6D6B1fCfDd2141a143b8";
