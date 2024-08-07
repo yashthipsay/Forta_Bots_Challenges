7a3c28c680307364c131ecbc9f36d328b318db06
Yash Thipsay <yashthpsay@gmail.com>
2024-08-07 19:48:11 +0530

    challenge-2

---
+++ Challenge-2/README.md
@@ -0,0 +1,30 @@
+# Uniswap Swap Detection Bot
+
+## Description
+
+This bot detects swaps in UniswapV3 Pool contracts on Ethereum, Polygon.
+
+## Supported Chains
+
+- Polygon 
+- Ethereum
+
+
+## Alerts
+
+Describe each of the type of alerts fired by this agent
+
+- `NETHERMIND-1`
+  - Address of the first token in the swap
+  - Address of the second token in the swap
+  - Fee required for the swap
+  - Amount for token 1
+  - Amount for token 2
+
+## Test Data
+
+The bot behaviour can be verified with the following transactions:
+
+[0x76eda3e5118fda2ec63ba850da54f9305c11a4712715180c8a97a54f2340b8fa](https://etherscan.io/tx/0x76eda3e5118fda2ec63ba850da54f9305c11a4712715180c8a97a54f2340b8fa)
+
+
---
+++ Challenge-2/src/constants.ts
@@ -0,0 +1,18 @@
+export const UNISWAP_FACTORY_ADDRESS = "0x1F98431c8aD98523631AE4a59f267346ea31F984";
+
+export const UNISWAP_FACTORY_ABI = [
+  `function getPool(address tokenA, address tokenB, uint24 fee) external view returns (address pool)`,
+  `event Swap(address sender, address recipient, int256 amount0, int256 amount1, uint160 sqrtPriceX96, uint128 liquidity, int24 tick)`,
+];
+
+export const UNISWAP_PAIR_ABI = [
+  "function token0() external view returns (address)",
+  "function token1() external view returns (address)",
+  "function fee() public view returns (uint24)",
+];
+
+export const COMPUTED_INIT_CODE_HASH = "0xe34f199b19b2b4f47f68442619d555527d244f78a3297ea89325f843f87b8b54";
+
+export const SWAP_EVENT = [
+  "event Swap(address indexed sender, address indexed recipient, int256 amount0, int256 amount1, uint160 sqrtPriceX96, uint128 liquidity, int24 tick)",
+];
