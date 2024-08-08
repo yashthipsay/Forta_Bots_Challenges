7240ff8e1f1f470fd726cc1e482f7860e3f196cc
Yash Thipsay <yashthpsay@gmail.com>
2024-08-08 08:52:22 +0530

    fetching collateral names only if not stored

--- Challenge-4/Bot_1/src/helper.ts
+++ Challenge-4/Bot_1/src/helper.ts
@@ -1,15 +1,13 @@
 import { ethers } from "forta-agent";
 import { LRUCache } from "lru-cache";
 import {
-  CONFIGURATOR_PROXY,
-  CONFIGURATOR_PROXY_ARB,
   ERC20_ABI,
 } from "./constants";
 
 export default class Helper {
   private provider: ethers.providers.Provider;
   private collateralAssetsCache: LRUCache<string, { [name: string]: string }>;
-  private collateralNamesCache: LRUCache<string, string>;
+  private collateralNamesCacheObject: { [address: string]: string};
 
   constructor(provider: ethers.providers.Provider) {
     this.provider = provider;
@@ -19,9 +17,7 @@ export default class Helper {
     >({
       max: 1000,
     });
-    this.collateralNamesCache = new LRUCache<string, string>({
-      max: 1000,
-    });
+    this.collateralNamesCacheObject = {}
   }
 
   public async getCollateralAssets(
@@ -44,17 +40,18 @@ export default class Helper {
     while (true) {
       try {
         let collateralAssetInfo: any;
-
+    
         collateralAssetInfo = await assetContract.getAssetInfo(index, {
           blockTag: blockNumber,
         });
-
-        const collateralName =
-          "Collateral Asset - " +
-          (await this.getCollateralName(
-            collateralAssetInfo.asset,
-            blockNumber,
-          ));
+    
+        let collateralName = this.collateralNamesCacheObject[collateralAssetInfo.asset];
+    
+        if (!collateralName) {
+          collateralName = "Collateral Asset - " + (await this.getCollateralName(collateralAssetInfo.asset, blockNumber));
+          this.collateralNamesCacheObject[collateralAssetInfo.asset] = collateralName;
+        }
+    
         collateralAssets[collateralName] = collateralAssetInfo.asset;
         index++; // Increment after successful fetch
       } catch (error) {
@@ -71,10 +68,7 @@ export default class Helper {
     tokenAddress: string,
     blockNumber: number,
   ): Promise<string | undefined> {
-    const key = `${tokenAddress}`;
-    if (this.collateralNamesCache.has(key)) {
-      return this.collateralNamesCache.get(key);
-    }
+    
     const tokenContract = new ethers.Contract(
       tokenAddress,
       ERC20_ABI,
@@ -82,7 +76,6 @@ export default class Helper {
     );
 
     const tokenName = await tokenContract.name({ blockTag: blockNumber });
-    this.collateralNamesCache.set(key, tokenName);
     return tokenName;
   }
 }
